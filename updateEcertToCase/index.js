5b12da24ea5a7ad3d78ed60d0c7b105bc37541cd
kellakaran15 <101532160+kellakaran15@users.noreply.github.com>
2022-04-19 14:39:20 +1000

    commit

--- updateEcertToCase/index.js
+++ updateEcertToCase/index.js
@@ -1,10 +1,20 @@
+const MongoClient = require('../api/dbConnect');
+const { config } = require('../config');
+const { ObjectId } = require('mongodb')
+
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const mongoClient = await MongoClient;
+    const db = await mongoClient.db(config.MONGO_DB);
+    const collection = db.collection('caseHeader');
+
+    const eCert = JSON.parse(req.body.eCertificate);
+    
+    const eCertificate = await collection.updateOne({ id: req.body.caseId }, { $set: eCert });
+
+    const responseMessage = eCertificate;
 
     context.res = {
         // status: 200, /* Defaults to 200 */
